a5879601f10eae4d6f7beb7be78180baf289d256
asison19 <andrewsison19@gmail.com>
2025-01-10 20:42:02 -0800

    feat(k8s): Add gke terraform resources

---
+++ kubernetes/main.tf
@@ -0,0 +1,33 @@
+resource "google_service_account" "default" {
+  account_id   = "gke-service-account"
+  display_name = "Service Account"
+}
+
+resource "google_container_cluster" "primary" {
+  name = "primary-gke-cluster"
+
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  location = "us-central1-a"
+
+  #network    = google_compute_network.default.id
+  #subnetwork = google_compute_subnetwork.default.id
+}
+
+resource "google_container_node_pool" "primary" {
+  name       = "primary-node-pool"
+  cluster    = google_container_cluster.primary.name
+  node_count = 1
+
+  node_config {
+    spot         = true
+    machine_type = "e2-small" # TODO create custom machine type?
+
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    service_account = google_service_account.default.email
+    oauth_scopes    = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+}
